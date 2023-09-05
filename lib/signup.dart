1651f20256b1703294f017931694f011825c8945
Peter Njuguna <petermwaurapeter254@gmail.com>
2023-09-05 23:46:53 +0300

    Signup form validation

--- lib/signup.dart
+++ lib/signup.dart
@@ -2,131 +2,202 @@ import 'package:flutter/material.dart';
 import 'package:job_search/forgotpassword.dart';
 import 'package:job_search/login.dart';
 
-
 void main() {
   runApp(
-    const MaterialApp(home: Signup()), // use MaterialApp
+     const MaterialApp(home: Signup()), // use MaterialApp
   );
 }
 
-class Signup extends StatelessWidget {
+class Signup extends StatefulWidget {
   const Signup({Key? key}) : super(key: key);
+
+  @override
+  _SignupState createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Container(
-          child:Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 80),
-              const Image(image: AssetImage('images/signup.png'),height: 200),
-              const SizedBox(height: 10),
-              const Text("Welcome!",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w800,fontSize: 40)),
-              const SizedBox(height: 10),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10), // Adjust the padding as needed
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(),
-                    hintText: 'example@email.com',
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10), // Adjust the padding as needed
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock_clock_outlined),
-                    iconColor: Colors.blue,
-                    border: OutlineInputBorder(),
-                    hintText: 'Password',
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10), // Adjust the padding as needed
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock_clock_outlined),
-                    iconColor: Colors.blue,
-                    border: OutlineInputBorder(),
-                    hintText: 'Confirm Password',
-                  ),
-                ),
-              ),
-              const SizedBox(height: 8),
-              Padding(padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child:   Align(
-                    alignment: Alignment.centerRight,
-                    child: Column(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => const Login()),
-                            );
-                          },
-                          child: const Text(
-                            "go to login?",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )),
-              const SizedBox(height: 10),
-              const Padding(padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 80),
+            const Image(image: AssetImage('images/signup.png'), height: 200),
+            const SizedBox(height: 10),
+            const Text("Welcome!",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 40)),
+            const SizedBox(height: 10),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Form(
+                key: _formKey,
                 child: Column(
                   children: [
-                    TextButton(
-                      onPressed: null,
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll(Colors.blue),
-                        minimumSize: MaterialStatePropertyAll(Size(double.infinity, 50)), // Adjust the height as needed
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.person),
+                        border: OutlineInputBorder(),
+                        hintText: 'example@email.com',
                       ),
-                      child: Text(
-                        "SIGN UP",
-                        style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),
-                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
                     ),
-                    SizedBox(height: 10),
-                    Text("Or Connect with media"),
-                    SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Expanded(
-                            child: TextButton(
-                                style: ButtonStyle(
-                                  backgroundColor: MaterialStatePropertyAll(Colors.blueAccent),
-                                ),
-                                onPressed: null,
-                                child: Text("Facebook",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),))),
-                        SizedBox(width: 20),
-                        Expanded(
-                            child: TextButton(
-                                style: ButtonStyle(
-                                  backgroundColor: MaterialStatePropertyAll(Colors.redAccent),
-                                ),
-                                onPressed: null, child: Text("Google",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),))
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.lock_clock_outlined),
+                        iconColor: Colors.blue,
+                        border: OutlineInputBorder(),
+                        hintText: 'Password',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: _confirmPasswordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.lock_clock_outlined),
+                        iconColor: Colors.blue,
+                        border: OutlineInputBorder(),
+                        hintText: 'Confirm Password',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please confirm your password';
+                        } else if (value != _passwordController.text) {
+                          return 'Passwords do not match';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 8),
+                    TextButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          // Form is valid, perform form submission logic here
+                          // For example, you can access form values using
+                          // _emailController.text, _passwordController.text, etc.
+                          print('Form is valid');
+                        }
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                        MaterialStateProperty.all(Colors.blue),
+                        minimumSize:
+                        MaterialStateProperty.all(Size(double.infinity, 50)),
+                      ),
+                      child: const Text(
+                        "SIGN UP",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Login()),
+                        );
+                      },
+                      child: const Text(
+                        "go to login?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
                         ),
-                      ],
+                      ),
                     ),
                   ],
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+            const SizedBox(height: 10),
+             Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  const Text("Or Connect with media"),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                            MaterialStateProperty.all(Colors.blueAccent),
+                          ),
+                          onPressed: null,
+                          child: const Text("Facebook",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: TextButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                            MaterialStateProperty.all(Colors.redAccent),
+                          ),
+                          onPressed: null,
+                          child: const Text("Google",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
